f273ecf49f35dfe17e0d1db2ec632c3ae407ccde
Oscar Menacho Silva <osqui.menacho2002@gmail.com>
2026-04-21 13:33:35 -0400

    initial commit

---
+++ src/test/java/ucb/sqm/tdd/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.sqm.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
